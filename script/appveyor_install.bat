c948bfb78f6b5a18b7299614c1118023b21877cd
Alex Robenko <arobenko@gmail.com>
2023-12-29 10:53:54 +1000

    Updating appveyor_install.bat

--- script/appveyor_install.bat
+++ script/appveyor_install.bat
@@ -1,7 +1,7 @@
 IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2017" (
     set TOOLCHAIN=msvc15
     set BOOST_VER=1_69_0
-    set CMAKE_GENERATOR="Visual Studio 15 2017"
+    set CMAKE_GENERATOR=Visual Studio 15 2017
 
     IF "%PLATFORM%"=="x86" (
         echo Performing x86 build in VS2017
@@ -14,7 +14,7 @@ IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2017" (
 ) ELSE IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2019" (
     set TOOLCHAIN=msvc16
     set BOOST_VER=1_77_0
-    set CMAKE_GENERATOR="Visual Studio 16 2019"
+    set CMAKE_GENERATOR=Visual Studio 16 2019
 
     IF "%PLATFORM%"=="x86" (
         echo Performing x86 build in VS2019
@@ -27,7 +27,7 @@ IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2017" (
 ) ELSE IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2022" (
     set TOOLCHAIN=msvc17
     set BOOST_VER=1_83_0
-    set CMAKE_GENERATOR="Visual Studio 17 2022"
+    set CMAKE_GENERATOR=Visual Studio 17 2022
 
     IF "%PLATFORM%"=="x86" (
         echo Performing x86 build in VS2022
@@ -42,9 +42,9 @@ IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2017" (
 )
 
 IF "%PLATFORM%"=="x86" (
-    set CMAKE_PLATFORM="Win32"
+    set CMAKE_PLATFORM=Win32
 ) ELSE (
-    set CMAKE_PLATFORM="x64"
+    set CMAKE_PLATFORM=x64
 )
 
 set BOOST_DIR=C:\Libraries\boost_%BOOST_VER%

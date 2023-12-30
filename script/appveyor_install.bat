b8fb17c575c272cd05f0b3830be120691a139a03
Alex Robenko <arobenko@gmail.com>
2023-12-30 10:22:27 +1000

    Improving appveyor configuration.

--- script/appveyor_install.bat
+++ script/appveyor_install.bat
@@ -1,11 +1,12 @@
 IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2017" (
     set TOOLCHAIN=msvc15
     set BOOST_VER=1_69_0
-    set CMAKE_GENERATOR=Visual Studio 15 2017
+    set CMAKE_GENERATOR=NMake Makefiles
 
     IF "%PLATFORM%"=="x86" (
         echo Performing x86 build in VS2017
         call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars32.bat"
+        
     ) ELSE (
         echo Performing amd64 build in VS2017
         call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
@@ -19,9 +20,11 @@ IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2017" (
     IF "%PLATFORM%"=="x86" (
         echo Performing x86 build in VS2019
         call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
+        set CMAKE_PLATFORM=Win32
     ) ELSE (
         echo Performing amd64 build in VS2019
         call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+        set CMAKE_PLATFORM=x64
     )
 
 ) ELSE IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2022" (
@@ -32,19 +35,15 @@ IF "%APPVEYOR_BUILD_WORKER_IMAGE%"=="Visual Studio 2017" (
     IF "%PLATFORM%"=="x86" (
         echo Performing x86 build in VS2022
         call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
+        set CMAKE_PLATFORM=Win32
     ) ELSE (
         echo Performing amd64 build in VS2022
         call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+        set CMAKE_PLATFORM=x64
     )        
 ) ELSE (
     echo Toolchain %TOOLCHAIN% is not supported
     exit -1
-)
-
-IF "%PLATFORM%"=="x86" (
-    set CMAKE_PLATFORM=Win32
-) ELSE (
-    set CMAKE_PLATFORM=x64
 )
 
 set BOOST_DIR=C:\Libraries\boost_%BOOST_VER%

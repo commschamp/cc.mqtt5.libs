eb6fa36cd27636508a0bfc9aff8bba08818ece2f
Alex Robenko <arobenko@gmail.com>
2024-03-06 08:57:25 +1000

    Requiring v2.6 of the cc.mqtt5.generated project.

--- client/lib/src/ProtocolDefs.h
+++ client/lib/src/ProtocolDefs.h
@@ -19,7 +19,7 @@
 
 #include <cstdint>
 
-static_assert(CC_MQTT5_VERSION <= COMMS_MAKE_VERSION(2, 5, 0), 
+static_assert(CC_MQTT5_VERSION <= COMMS_MAKE_VERSION(2, 6, 0), 
     "The version of the cc_mqtt5 library is too low.");
 
 namespace cc_mqtt5_client

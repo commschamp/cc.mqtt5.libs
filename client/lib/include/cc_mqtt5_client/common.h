a83a92173fab0a7aaca862b310d8841d81868d84
Alex Robenko <arobenko@gmail.com>
2024-04-16 07:48:36 +1000

    Cosmetic documentation updates.

--- client/lib/include/cc_mqtt5_client/common.h
+++ client/lib/include/cc_mqtt5_client/common.h
@@ -6,7 +6,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 /// @file
-/// @brief Common definition for MQTT-SN clients.
+/// @brief Common definition for MQTT v5 clients.
 
 #pragma once
 

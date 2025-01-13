677518128be883a98b5be2b302e9279ea8501559
Alex Robenko <arobenko@gmail.com>
2025-01-14 08:54:41 +1000

    Updating copyright years to include 2025.

--- client/lib/test/unit/UnitTestProtocolDefs.h
+++ client/lib/test/unit/UnitTestProtocolDefs.h
@@ -1,10 +1,3 @@
-//
-// Copyright 2023 - 2024 (C). Alex Robenko. All rights reserved.
-//
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
 #pragma once
 
 #include "cc_mqtt5/Message.h"
